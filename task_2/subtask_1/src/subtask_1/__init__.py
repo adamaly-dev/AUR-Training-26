f870528e67a6fb24588f046af54acb1d56107ceb
Adam Aly <adamaly243@gmail.com>
2026-08-26 23:10:42 +0300

    Cleaned the code a bit

--- task_2/subtask_1/src/subtask_1/__init__.py
+++ task_2/subtask_1/src/subtask_1/__init__.py
@@ -4,6 +4,15 @@ def disp(stock_dict) -> None:
     for key, value in stock_dict.items():
         print(f"{key}: {value}")
 
+def inp_number() -> int:
+    try:
+        new_value = int(input("Enter the stock value: ").strip())
+    except:
+        raise ValueError("Stock value must be a positive integer")
+    if new_value <= 0:
+        raise ValueError("Stock value must be a positive integer")
+    return new_value
+
 def main() -> None:
     stock_dict = {}
 
@@ -23,28 +32,22 @@ def main() -> None:
             query_type = int(input("Enter query type (1-4): ").strip())
             if query_type == 1:
                 disp(stock_dict)
+
                 new_key = input("Enter a stock type: ").strip().lower()
-                try:
-                    new_value = int(input("Enter the stock value: ").strip())
-                except:
-                    raise ValueError("Stock value must be a positive integer")
-                if new_value <= 0:
-                    raise ValueError("Stock value must be a positive integer")
+                new_value = inp_number()
+
                 if new_key in stock_dict:
                     stock_dict[new_key] += new_value
                 else:
                     stock_dict[new_key] = new_value
             elif query_type == 2:
                 disp(stock_dict)
+
                 new_key = input("Enter a stock type: ").strip().lower()
                 if (new_key not in stock_dict):
                     raise ValueError("Stock doesn't exist")
-                try:
-                    new_value = int(input("Enter the stock value: ").strip())
-                except:
-                    raise ValueError("Stock value must be a positive integer")
-                if new_value <= 0:
-                    raise ValueError("Stock value must be a positive integer")
+                new_value = inp_number()
+
                 if stock_dict[new_key] < new_value:
                     raise ValueError(f"Stock value to be removed is higher than the current stock value")
                 stock_dict[new_key] -= new_value
