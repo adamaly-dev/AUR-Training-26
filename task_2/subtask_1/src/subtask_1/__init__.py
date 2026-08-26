5a36a8519344c84260d388fa385b17a74ac3ec1b
Adam Aly <adamaly243@gmail.com>
2026-08-26 22:56:11 +0300

    Implemented remove query, display query, and exit query

--- task_2/subtask_1/src/subtask_1/__init__.py
+++ task_2/subtask_1/src/subtask_1/__init__.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+def disp(stock_dict) -> None:
+    for key, value in stock_dict.items():
+        print(f"{key}: {value}")
+
 def main() -> None:
     stock_dict = {}
 
@@ -18,23 +22,39 @@ def main() -> None:
         try:
             query_type = int(input("Enter query type (1-4): ").strip())
             if query_type == 1:
-                for key, value in stock_dict.items():
-                    print(f"{key}: {value}")
+                disp(stock_dict)
                 new_key = input("Enter a stock type: ").strip().lower()
                 try:
                     new_value = int(input("Enter the stock value: ").strip())
                 except:
-                    raise ValueError("Stock value must be an integer")
+                    raise ValueError("Stock value must be a positive integer")
+                if new_value <= 0:
+                    raise ValueError("Stock value must be a positive integer")
                 if new_key in stock_dict:
                     stock_dict[new_key] += new_value
                 else:
                     stock_dict[new_key] = new_value
             elif query_type == 2:
-                pass
+                disp(stock_dict)
+                new_key = input("Enter a stock type: ").strip().lower()
+                if (new_key not in stock_dict):
+                    raise ValueError("Stock doesn't exist")
+                try:
+                    new_value = int(input("Enter the stock value: ").strip())
+                except:
+                    raise ValueError("Stock value must be a positive integer")
+                if new_value <= 0:
+                    raise ValueError("Stock value must be a positive integer")
+                if stock_dict[new_key] < new_value:
+                    raise ValueError(f"Stock value to be removed is higher than the current stock value")
+                stock_dict[new_key] -= new_value
             elif query_type == 3:
-                pass
+                disp(stock_dict)
             elif query_type == 4:               
-                pass         
+                with open(file_path, "w") as f:
+                    for key, value in stock_dict.items():
+                        f.write(key+","+str(value)+"\n")
+                break
             else:
                 raise ValueError("Query type must be between (1-4)")
         except:
