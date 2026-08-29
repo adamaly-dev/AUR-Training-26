6fa48531852e097b0086d5200ba1238f29fdf430
Adam Aly <adamaly243@gmail.com>
2026-08-29 11:14:20 +0300

    ISBN Validation

--- task_3/src/task_3/__init__.py
+++ task_3/src/task_3/__init__.py
@@ -55,7 +55,6 @@ class LibraryItem:
                 words = [word.strip() for word in re.split(r'[|=]', line)]
                 data = dict(zip(words[2::2], words[3::2]))
                 data["id"] = len(library)
-
                 if words[1] == "Book":
                     library.append(Book(**data))
                 elif words[1] == "DVD":
@@ -65,17 +64,28 @@ class LibraryItem:
                 else:
                     raise TypeError("Library Item Type does not exist")
 
-
 class Book(LibraryItem):
     loan_days = 21
 
     def __init__(self, id:int, title:str, author:str, isbn:str, status:ItemStatus):
+        if not Book.validateISBN(isbn):
+            raise ValueError("Invalid ISBN")
         super().__init__(id, title, status)
         self.isbn = isbn
         self.author = author
 
     def __repr__(self):
         return f"ID: #{self.id}, {self.title} (Book) - {self._status.name}"
+    
+    @staticmethod
+    def validateISBN(isbn:str):
+        if len(isbn) != 13 or not isbn.isdigit():
+            return False
+        sum = 0
+        for i in range(0, 13):
+            sum += (int(isbn[i])-int('0'))*(i%2*2+1)
+        sum %= 10
+        return (sum == 0)
 
 class DVD(LibraryItem):
     loan_days = 5
