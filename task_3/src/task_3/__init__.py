566dbdb35ea7fbf79f2b2953248b97dd5b91a840
Adam Aly <adamaly243@gmail.com>
2026-08-29 10:54:37 +0300

    Fixed a bug in ItemStatus, Implemented a class method to take the library data from a text file

--- task_3/src/task_3/__init__.py
+++ task_3/src/task_3/__init__.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from pathlib import Path
+import re
 
 class ItemStatus(Enum):
     AVAILABLE = 1
@@ -9,20 +11,23 @@ class LibraryItem:
     loan_days:int
     _status:ItemStatus
     id:int
-    name:str
+    title:str
 
-    def __init__(self, name, id):
-        self._status = ItemStatus.AVAILABLE
+    def __init__(self, id:int, title:str, status:ItemStatus):
         self.id = id
-        self.name = name
+        self.title = title
+        self._status = ItemStatus[status]
+
+    def __lt__(self, other:"LibraryItem"):
+        return self.title < other.title
 
     @property
-    def status(self):
+    def status(self) -> ItemStatus:
         return self._status
     
     @status.setter
-    def status(self):
-        raise NotImplementedError()
+    def status(self, new_status:ItemStatus):
+        self._status = new_status
 
     def checkout(self):
         if self._status != ItemStatus.AVAILABLE:
@@ -37,28 +42,70 @@ class LibraryItem:
         print(f"Return of item {self.id} has been successful")
 
     def mark_lost(self):
-        if self._status != ItemStatus.CHECKED_OUT:
-            raise ValueError("Item is in library or is already reported lost")
         self._status = ItemStatus.LOST
         print(f"Item {self.id} is lost")
+
+    @classmethod
+    def from_dict(cls, library:list["LibraryItem"]):
+        file_path = Path(__file__).parent / "database.txt"
+        with open(file_path, "r") as f:
+            for line in f:
+                if not line:
+                    continue    
+                words = [word.strip() for word in re.split(r'[|=]', line)]
+                data = dict(zip(words[2::2], words[3::2]))
+                data["id"] = len(library)
+
+                if words[1] == "Book":
+                    library.append(Book(**data))
+                elif words[1] == "DVD":
+                    library.append(DVD(**data))
+                elif words[1] == "Magazine":
+                    library.append(Magazine(**data))
+                else:
+                    raise TypeError("Library Item Type does not exist")
+
 
 class Book(LibraryItem):
     loan_days = 21
 
+    def __init__(self, id:int, title:str, author:str, isbn:str, status:ItemStatus):
+        super().__init__(id, title, status)
+        self.isbn = isbn
+        self.author = author
+
+    def __repr__(self):
+        return f"ID: #{self.id}, {self.title} (Book) - {self._status.name}"
+
 class DVD(LibraryItem):
     loan_days = 5
+
+    def __init__(self, id:int, title:str, director:str, status:ItemStatus):
+        super().__init__(id, title, status)
+        self.director = director
+
+    def __repr__(self):
+        return f"ID: #{self.id}, {self.title} (DVD) - {self._status.name}"
 
 class Magazine(LibraryItem):
     loan_days = 14
 
+    def __init__(self, id, title, issue, status):
+        super().__init__(id, title, status)
+        self.issue = issue
+
+    def __repr__(self):
+        return f"ID: #{self.id}, {self.title} (Magazine) - {self._status.name}"
+
 def main() -> None:
-    library:LibraryItem = [Book("Harry Potter", 0),
-                            DVD("Sonic", 1), 
-                            DVD("Mario", 2), 
-                            Magazine("The Atlantic", 3), 
-                            Book("Dunes", 4)]
+    library:LibraryItem = []
+    LibraryItem.from_dict(library)
     while True:
-        query = int(input("Query type (1/2/3): ").strip())
+        query = int(input("Query type (1/2/3/4): ").strip())
+        if query == 4:
+            for item in library:
+                print(repr(item))
+            continue
         item_id = int(input("Item ID: ").strip())
         if item_id >= len(library):
             raise("Item does not exist")
